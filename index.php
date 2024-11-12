24b955166bcd12625cfc36af792c63075345ba1f
rifani53 <muhammadrifani23@mhs.politala.ac.id>
2024-11-12 14:39:37 +0800

    add:.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     echo "Hello World!";
     echo "kelas 3B - IT Proyek";
     echo "Siti Rukmana";
-    echo "Rifani";
+    echo "Rifani.";
     echo "Sistem Integrasi";
     echo "Muhammad Fajar Ramadhan";
 ?>
